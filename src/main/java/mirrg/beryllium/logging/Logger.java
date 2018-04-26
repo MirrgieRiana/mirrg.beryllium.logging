43774d1cee6c44c9f85fc5358da78ece86008a67
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-04-26 13:08:00 +0900

    add: Logger, TaggedLogger

---
+++ src/main/java/mirrg/beryllium/logging/Logger.java
@@ -0,0 +1,105 @@
+package mirrg.beryllium.logging;
+
+import java.util.Optional;
+
+public class Logger
+{
+
+	private LogSink logSink;
+
+	public Logger(LogSink logSink)
+	{
+		this.logSink = logSink;
+	}
+
+	public synchronized void println(String tag, String string, Optional<EnumLogLevel> oLogLevel)
+	{
+		logSink.println(tag, string, oLogLevel);
+	}
+
+	public void println(String tag, String string)
+	{
+		println(tag, string, Optional.empty());
+	}
+
+	public void println(String tag, String string, EnumLogLevel logLevel)
+	{
+		println(tag, string, Optional.of(logLevel));
+	}
+
+	public void fatal(String tag, String string)
+	{
+		println(tag, string, EnumLogLevel.FATAL);
+	}
+
+	public void error(String tag, String string)
+	{
+		println(tag, string, EnumLogLevel.ERROR);
+	}
+
+	public void warn(String tag, String string)
+	{
+		println(tag, string, EnumLogLevel.WARN);
+	}
+
+	public void info(String tag, String string)
+	{
+		println(tag, string, EnumLogLevel.INFO);
+	}
+
+	public void debug(String tag, String string)
+	{
+		println(tag, string, EnumLogLevel.DEBUG);
+	}
+
+	public void trace(String tag, String string)
+	{
+		println(tag, string, EnumLogLevel.TRACE);
+	}
+
+	public synchronized void println(String tag, Throwable e, Optional<EnumLogLevel> oLogLevel)
+	{
+		logSink.println(tag, e, oLogLevel);
+	}
+
+	public void println(String tag, Throwable e)
+	{
+		println(tag, e, Optional.empty());
+	}
+
+	public void println(String tag, Throwable e, EnumLogLevel logLevel)
+	{
+		println(tag, e, Optional.of(logLevel));
+	}
+
+	public void fatal(String tag, Throwable e)
+	{
+		println(tag, e, EnumLogLevel.FATAL);
+	}
+
+	public void error(String tag, Throwable e)
+	{
+		println(tag, e, EnumLogLevel.ERROR);
+	}
+
+	public void warn(String tag, Throwable e)
+	{
+		println(tag, e, EnumLogLevel.WARN);
+	}
+
+	public void info(String tag, Throwable e)
+	{
+		println(tag, e, EnumLogLevel.INFO);
+	}
+
+	public void debug(String tag, Throwable e)
+	{
+		println(tag, e, EnumLogLevel.DEBUG);
+	}
+
+	public void trace(String tag, Throwable e)
+	{
+		println(tag, e, EnumLogLevel.TRACE);
+	}
+
+}
