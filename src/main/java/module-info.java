faeb6b37659066f33689c803eb2b34b38a6420ad
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-08-11 17:09:04 +0900

    format

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module mirrg.beryllium.logging
 {
 	exports mirrg.beryllium.logging;
-	exports mirrg.beryllium.logging.io;
 	exports mirrg.beryllium.logging.core;
+	exports mirrg.beryllium.logging.io;
 
 	requires transitive java.desktop;
 	requires static org.junit.jupiter.api;
